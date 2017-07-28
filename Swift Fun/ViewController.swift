2a39801bed9286ee7f800e87cbacae6fba552cd2
William Dawson <williamdawson@Williams-MacBook-Pro-2.local>
2017-07-27 21:01:44 -0400

    First time using commit file

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -9,10 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var buttonCount:Int = 0
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func onClick(_ sender: Any) {
+        
+        buttonCount = buttonCount + 1
+        
+        if buttonCount == 10 {
+            view.backgroundColor = UIColor.red  //change background color
+            myLabel.text = "Bill is cool!"  //changing the text of the label
+            
+            
+        }
+        
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
